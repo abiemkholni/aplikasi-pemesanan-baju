d530e8713bca9fe295db91bff7e629b714467a86
musyi04 <mursyidaam4@gmail.com>
2025-07-24 21:06:15 +0700

    aplikasi pemesanan baju

---
+++ Aplikassi pemesanan baju/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Aplikassi_pemesanan_baju
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textnama = new System.Windows.Forms.TextBox();
+            this.textsandi = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.movietimer = new System.Windows.Forms.Timer(this.components);
+            this.fadetimer = new System.Windows.Forms.Timer(this.components);
+            this.labelLoading = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textnama
+            // 
+            this.textnama.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textnama.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textnama.Location = new System.Drawing.Point(209, 243);
+            this.textnama.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textnama.Name = "textnama";
+            this.textnama.Size = new System.Drawing.Size(200, 15);
+            this.textnama.TabIndex = 1;
+            this.textnama.TextChanged += new System.EventHandler(this.textnama_TextChanged);
+            // 
+            // textsandi
+            // 
+            this.textsandi.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textsandi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textsandi.Location = new System.Drawing.Point(209, 309);
+            this.textsandi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textsandi.Name = "textsandi";
+            this.textsandi.Size = new System.Drawing.Size(200, 15);
+            this.textsandi.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(3, 450);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(625, 38);
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // movietimer
+            // 
+            this.movietimer.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // fadetimer
+            // 
+            this.fadetimer.Tick += new System.EventHandler(this.fadetimer_Tick);
+            // 
+            // labelLoading
+            // 
+            this.labelLoading.AutoSize = true;
+            this.labelLoading.BackColor = System.Drawing.Color.LimeGreen;
+            this.labelLoading.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLoading.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelLoading.Location = new System.Drawing.Point(-1, 460);
+            this.labelLoading.Name = "labelLoading";
+            this.labelLoading.Size = new System.Drawing.Size(76, 20);
+            this.labelLoading.TabIndex = 7;
+            this.labelLoading.Text = "Loading..";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Aplikassi_pemesanan_baju.Properties.Resources._247320;
+            this.pictureBox1.Location = new System.Drawing.Point(246, 36);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::Aplikassi_pemesanan_baju.Properties.Resources.SDFT___Copy;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(246, 328);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 74);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(232, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "SELAMAT DATANG";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(259, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "USERNAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(259, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "PASSWORD";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = global::Aplikassi_pemesanan_baju.Properties.Resources.WhatsApp_Image_2025_06_30_at_03_491;
+            this.ClientSize = new System.Drawing.Size(633, 493);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelLoading);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textsandi);
+            this.Controls.Add(this.textnama);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textnama;
+        private System.Windows.Forms.TextBox textsandi;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer movietimer;
+        private System.Windows.Forms.Timer fadetimer;
+        private System.Windows.Forms.Label labelLoading;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Aplikassi pemesanan baju/Form2.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Aplikassi_pemesanan_baju
+{
+    public partial class Form2 : Form
+    {
+        public string NamaPelanggan
+        {
+            get { return txtNama.Text; }
+        }
+
+        Dictionary<string, Dictionary<string, int>> hargaBaju = new Dictionary<string, Dictionary<string, int>>()
+        {
+            { "Kaos", new Dictionary<string, int> { { "S", 20000 }, { "M", 25000 }, { "L", 30000 } } },
+            { "Kemeja", new Dictionary<string, int> { { "S", 30000 }, { "M", 35000 }, { "L", 40000 } } },
+            { "Jaket", new Dictionary<string, int> { { "S", 40000 }, { "M", 45000 }, { "L", 50000 } } }
+        };
+
+        List<string> keranjang = new List<string>();
+        int totalHarga = 0;
+        int totalSetelahDiskon = 0;
+        int diskon = 0;
+        bool diskonSudahDiberikan = false; // Tambahan
+
+        private List<string> daftarMakananCheckout;
+        private int totalHargaCheckout;
+
+        public Form2()
+        {
+            InitializeComponent();
+            cmbItem.Items.AddRange(new string[] { "Kaos", "Kemeja", "Jaket" });
+            cmbUkuran.Items.AddRange(new string[] { "S", "M", "L" });
+            nudJumlah.Minimum = 0;
+        }
+
+        private void btnKeranjang_Click(object sender, EventArgs e)
+        {
+            string nama = txtNama.Text;
+            string itemDipilih = cmbItem.Text;
+            string ukuran = cmbUkuran.Text;
+            int jumlah = (int)nudJumlah.Value;
+
+            if (string.IsNullOrWhiteSpace(nama) || itemDipilih == "" || ukuran == "")
+            {
+                MessageBox.Show("Lengkapi data terlebih dahulu!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (jumlah <= 0)
+            {
+                MessageBox.Show("Jumlah harus lebih dari 0!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int hargaSatuan = hargaBaju[itemDipilih][ukuran];
+            int subtotal = hargaSatuan * jumlah;
+            totalHarga += subtotal;
+
+            string detailPesanan = $"{itemDipilih} - {ukuran} x{jumlah} = Rp{subtotal:N0}";
+            keranjang.Add(detailPesanan);
+            listBox1.Items.Add(detailPesanan);
+
+            int diskonSebelumnya = diskon;
+            diskon = (totalHarga > 50000) ? (int)(totalHarga * 0.15) : 0;
+            totalSetelahDiskon = totalHarga - diskon;
+
+            txtDiskon.Text = $"Rp{diskon:N0}";
+            txtHarga.Text = $"Rp{totalSetelahDiskon:N0}";
+
+            // Tampilkan MessageBox jika diskon pertama kali diberikan
+            if (diskon > 0 && !diskonSudahDiberikan)
+            {
+                MessageBox.Show("Selamat! Anda mendapatkan diskon 15% karena belanja di atas Rp50.000.", "Diskon Diterapkan", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                diskonSudahDiberikan = true;
+            }
+
+            TampilkanRingkasanPembayaran();
+        }
+
+        private void TampilkanRingkasanPembayaran()
+        {
+            // Hapus ringkasan lama dari listBox
+            for (int i = listBox1.Items.Count - 1; i >= 0; i--)
+            {
+                string isi = listBox1.Items[i].ToString();
+                if (isi.StartsWith("───") || isi.StartsWith("Harga Akhir") || isi.StartsWith("Diskon") || isi.StartsWith("Total IDR"))
+                {
+                    listBox1.Items.RemoveAt(i);
+                }
+            }
+
+            // Tambah ringkasan baru
+            listBox1.Items.Add("─── RINCIAN PEMBAYARAN ───");
+            listBox1.Items.Add($"Total IDR Rp: {totalSetelahDiskon:N0}");
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Tombol Checkout
+        {
+            if (keranjang.Count == 0)
+            {
+                MessageBox.Show("Belum ada pesanan dalam keranjang.", "Info");
+                return;
+            }
+
+            daftarMakananCheckout = new List<string>(keranjang);
+            totalHargaCheckout = totalSetelahDiskon;
+
+            progressBar1.Value = 0;
+            progressBar1.Visible = true;
+            labelloading.Visible = true;
+            labelloading.Left = -labelloading.Width;
+            movietimer.Start();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 100)
+            {
+                progressBar1.Value += 5;
+            }
+            else
+            {
+                timer1.Stop();
+                movietimer.Stop();
+                labelloading.Visible = false;
+                progressBar1.Visible = false;
+
+                Form3 form3 = new Form3(daftarMakananCheckout, totalHargaCheckout, txtNama.Text);
+                form3.Show();
+                this.Hide();
+            }
+        }
+
+        private void movietimer_Tick(object sender, EventArgs e)
+        {
+            labelloading.Left += 5;
+            if (labelloading.Left > this.Width)
+            {
+                labelloading.Left = -labelloading.Width;
+            }
+        }
+
+        private void cmbItem_SelectedIndexChanged(object sender, EventArgs e) => HitungHargaOtomatis();
+        private void cmbUkuran_SelectedIndexChanged(object sender, EventArgs e) => HitungHargaOtomatis();
+        private void nudJumlah_ValueChanged(object sender, EventArgs e) => HitungHargaOtomatis();
+
+        private void HitungHargaOtomatis()
+        {
+            string item = cmbItem.Text;
+            string ukuran = cmbUkuran.Text;
+            int jumlah = (int)nudJumlah.Value;
+
+            if (hargaBaju.ContainsKey(item) && hargaBaju[item].ContainsKey(ukuran) && jumlah > 0)
+            {
+                int hargaSatuan = hargaBaju[item][ukuran];
+                int subtotal = hargaSatuan * jumlah;
+                txtHarga.Text = $"Rp{subtotal:N0}";
+            }
+            else
+            {
+                txtHarga.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Bersihkan keranjang
+        {
+            keranjang.Clear();
+            listBox1.Items.Clear();
+            totalHarga = 0;
+            diskon = 0;
+            totalSetelahDiskon = 0;
+            diskonSudahDiberikan = false; // reset
+            txtDiskon.Clear();
+            txtHarga.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Reset form input
+        {
+            cmbItem.SelectedIndex = -1;
+            cmbUkuran.SelectedIndex = -1;
+            nudJumlah.Value = 0;
+            txtHarga.Clear();
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            progressBar1.Visible = false;
+            labelloading.Visible = false;
+            this.Opacity = 1.0;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
